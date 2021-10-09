1058651e8cade0041a357c66bc8bdb4317f031a8
Sergei <hsfk@yandex.ru>
2021-10-08 20:03:51 -0700

    client: Catch errors in 'fetchCurrentUser'

--- client/src/api/user.ts
+++ client/src/api/user.ts
@@ -2,5 +2,6 @@ import { User } from '../../../common/api/models';
 import api from './api';
 
 export async function fetchCurrentUser(): Promise<User | undefined> {
-    return api.get('/user/me', { withCredentials: true }).then(x => x.data === null ? undefined : x.data);
+    return api.get('/user/me', { withCredentials: true })
+        .then(x => x.data).catch(_ => undefined);
 }
